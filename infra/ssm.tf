0784c02485a173ff88f5619d50e4b7e055a69173
Raket Makhim <95826767+raketmakhim@users.noreply.github.com>
2026-03-21 23:06:59 +0000

    tell tf to ignore ssm pram lifecycel (#2)

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -2,28 +2,33 @@ resource "aws_ssm_parameter" "viewer_username" {
   name  = "/family-tree/viewer-username"
   type  = "String"
   value = var.viewer_username
+  lifecycle { ignore_changes = [value] }
 }
 
 resource "aws_ssm_parameter" "viewer_password" {
   name  = "/family-tree/viewer-password"
   type  = "SecureString"
   value = var.viewer_password
+  lifecycle { ignore_changes = [value] }
 }
 
 resource "aws_ssm_parameter" "editor_username" {
   name  = "/family-tree/editor-username"
   type  = "String"
   value = var.editor_username
+  lifecycle { ignore_changes = [value] }
 }
 
 resource "aws_ssm_parameter" "editor_password" {
   name  = "/family-tree/editor-password"
   type  = "SecureString"
   value = var.editor_password
+  lifecycle { ignore_changes = [value] }
 }
 
 resource "aws_ssm_parameter" "jwt_secret" {
   name  = "/family-tree/jwt-secret"
   type  = "SecureString"
   value = var.jwt_secret
+  lifecycle { ignore_changes = [value] }
 }
